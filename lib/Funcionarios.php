9879d2401e160dfea9923abe5245683910727d60
Tales Andrade <tales.andrad@gmail.com>
2024-01-16 14:54:30 -0300

    Correção de erro no método pegarFuncionario para retornar todos funcionários disponíveis.

--- lib/Funcionarios.php
+++ lib/Funcionarios.php
@@ -29,7 +29,7 @@ class Funcionarios
     public function pegarFuncionario()
     {
         if (isset($this->funcionarios[$this->indice++])) {
-            return $this->funcionarios[$this->indice++];
+            return $this->funcionarios[$this->indice];
         }
     }
 }
