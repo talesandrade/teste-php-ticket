abab0711990f7be1d2c3cd658c8c991ea7f8a86a
Tales Andrade <tales.andrad@gmail.com>
2024-01-16 17:55:30 -0300

    Criação de método para importação de funcionários.

--- lib/Funcionarios.php
+++ lib/Funcionarios.php
@@ -23,27 +23,72 @@ class Funcionarios
         $this->funcionarios[] = new Funcionario($x++, 'Carmiro José', K_FUNCIONARIO_DESONESTO);
         $this->funcionarios[] = new Funcionario($x++, 'José Maria', K_FUNCIONARIO_HONESTO);
         $this->funcionarios[] = new Funcionario($x++, 'Dinho', K_FUNCIONARIO_DESONESTO);
+
+        $this->importarFuncionarios($x);
     }
 
     #Método retorna todos os funcionários disponíveis um por vez.
     public function pegarFuncionario()
     {
-        if (isset($this->funcionarios[$this->indice++])) {
-            return $this->funcionarios[$this->indice];
+        if (isset($this->funcionarios[$this->indice])) {
+            return $this->funcionarios[$this->indice++];
         }
     }
 
     # Método que retorna apenas o funciorário do referente ao id informado.
     public function pegarFuncionarioPorId($id)
     {
-        if(isset($this->funcionarios[$id]))
+        if(isset($this->funcionarios[$id-1]))
         {
-            return $this->funcionarios[$id];
+            return $this->funcionarios[$id-1];
 
         }else{
 
            return('Este funcionário não está disponível.');
            
+        }
+    }
+
+    public function importarFuncionarios($ultimoId)
+    {
+        $funcionarios = file("candidatos.dump", FILE_IGNORE_NEW_LINES);
+
+        foreach ($funcionarios as $linha) {
+
+            $primeiroChar = substr($linha, 0, 1);
+
+            if($primeiroChar != "#" && $primeiroChar != ""){
+                $funcionarioImportado = explode("\t",$linha);
+                $this->funcionarios[] = new Funcionario($ultimoId, $funcionarioImportado[1], $this->converterTipo($funcionarioImportado[2]));
+                $ultimoId++;
+            }
+
+        }
+
+        return $this->funcionarios;
+    }
+
+    public function converterTipo($tipo)
+    {
+        switch ($tipo) {
+            case 'm':
+                return 1;
+                break;
+            case 'e':
+                return 2;
+                break;
+            case 'h':
+                return 3;
+                break;
+            case 'd':
+                return 4;
+                break;
+            case 'p':
+                return 5;
+                break;
+            case 'c':
+                return 6;
+                break;
         }
     }
 }
